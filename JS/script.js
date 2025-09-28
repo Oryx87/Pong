1a55ae371604f744e2224cf8d3a60a61232cc975
Mylan <mylan.baudin-marie@etu.unicaen.fr>
2025-09-28 18:20:15 +0200

    Ajout déplacement balle aléatoire, affichage partie perdue

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,6 @@
 const game = document.getElementById("game");
 const ctx = game.getContext('2d');
 
-// ctx.beginPath();
-// ctx.moveTo(raquetteX, raquetteY);
-// ctx.lineTo(raquetteX + raquetteWidth, raquetteY);
-// ctx.strokeStyle = 'white';
-// ctx.lineWidth = 10;
-// ctx.stroke();
-// ctx.closePath();
-
 function dessinePlateau(){
     ctx.fillStyle = 'black';
     ctx.strokeStyle = 'white';
@@ -21,7 +13,7 @@ const raquetteWidth = 100;
 const raquetteHeight = 10;
 const raquetteY = game.height - 25;
 let raquetteX = game.width/2 - 50;
-let raquetteSpeed = 10;
+let raquetteSpeed = 20;
 
 function dessineRaquette(){
     ctx.fillStyle = "white";
@@ -29,8 +21,8 @@ function dessineRaquette(){
 }
 
 //Coordonnées Balle
-const balleX = game.width / 2;
-const balleY = game.height - 50;
+let balleX = game.width / 2;
+let balleY = game.height - 50;
 const balleRadius = 10;
 
 function dessineBalle(){
@@ -71,38 +63,94 @@ document.addEventListener("keydown", (e) => {
     afficherJeu();
 });
 
-
-//Mouvement de la balle
-// Pour gérer les déplacements dans le canvas
-// request animation frame
-// cancel animation frame
-function moveBall(){
-
+function randomDeplacement(balleSpeed){
+    const angle = (Math.random() * (150 - 30) + 30) * Math.PI / 180;
+    return {
+        x: balleSpeed * Math.cos(angle) * (Math.random() < 0.5 ? -1 : 1),
+        y: -balleSpeed * Math.sin(angle)
+    }
 }
 
-moveBall();
+let balleXSpeed;
+let balleYSpeed;
+let rafID;
+const result = document.getElementById('resultat');
+let temps = 0;
 
+function moveBalle(timestamp){
+    balleX += balleXSpeed;
+    balleY += balleYSpeed;
 
+    //Collision Bordure Gauche/Droite
+    if ( balleX + balleRadius > game.width || balleX - balleRadius < 0){
+        balleXSpeed = -balleXSpeed;
+    }
 
+    //Collision Bordure Haute
+    if ( balleY - balleRadius < 0 ){
+        balleYSpeed = -balleYSpeed;
+    }
+
+    //Collision Raquette
+    if ( balleX + balleRadius > raquetteX && balleY + balleRadius >= raquetteY && balleX - balleRadius < raquetteX + raquetteWidth && balleY - balleRadius <= raquetteY + raquetteHeight){
+        balleYSpeed = -balleYSpeed;
+        balleY = raquetteY - balleRadius;
+    }
+
+    //Condition partie perdue
+    if ( balleY - balleRadius > game.height){
+        cancelAnimationFrame(rafID);
+        clearInterval(timerID);
+        result.innerHTML = `<br>Vous avez perdu au bout de ${temps} secondes !<br>
+        Vous pouvez faire mieux`
+        return;
+    }
+    afficherJeu();
+
+    rafID = requestAnimationFrame(moveBalle);
+}
 
 const newGameButton = document.getElementById('newGame');
 const scoreDisplay = document.getElementById('score');
-let temps = 0;
+let timerID;
 let nbGame = 0;
 
 //Button newGame
-function timer(){
-    scoreDisplay.innerHTML = `Score : ${temps} s`
-    temps++;
+
+function startTimer() {
+    scoreDisplay.textContent = `Score : 0 s`;
+    timerID = setInterval(() => {
+        temps++;
+        scoreDisplay.textContent = `Score : ${temps} s`;
+    }, 1000);
 }
 
 newGameButton.addEventListener('click', () => {
     if ( nbGame === 0){
-        setInterval(timer,1000);
+        const direction = randomDeplacement(3);
+        balleXSpeed = direction.x;
+        balleYSpeed = direction.y;
+        startTimer();
         nbGame++;
+        rafID = requestAnimationFrame(moveBalle);
     } else {
+        cancelAnimationFrame(rafID);
+        clearInterval(timerID);
+
+        balleX = game.width / 2;
+        balleY = game.height - 50;
+
+        const direction = randomDeplacement(3);
+        balleXSpeed = direction.x;
+        balleYSpeed = direction.y;
+
+        raquetteX = game.width/2 - 50;
         temps = 0;
-        nbGame++;
+        result.textContent = '';
+        afficherJeu();
+
+        startTimer();
+        rafID = requestAnimationFrame(moveBalle);
     }
 })
 
@@ -111,4 +159,9 @@ newGameButton.addEventListener('click', () => {
 // Trucs a rajouter :
 // - Bordure arrondis plateau + raquette
 // - lissage déplacement ( + fluide )
-// - fonctionnalités à faire 
+//Fonctionnalités à faire :
+// - commandes touch
+// - vitesse balle augmente progressivement (max x5)
+// - déplacement peut pas etre vertical ( 90° raquette)
+// - stocker meilleur score
+// +Bonus...
